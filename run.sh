729f358dc3ce282125b2331ed881cbd8ab4c243d
sara-02 <sarahmasud02@gamil.com>
2017-09-21 23:56:02 +0530

    Fix error whiel running run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python ./src/python/run_stream.py ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json
+PYTHONPATH=`pwd` python ./src/python/run_stream.py ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json
