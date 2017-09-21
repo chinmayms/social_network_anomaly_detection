59c6decaed166970951c58a4482c90357a249dfd
sara-02 <sarahmasud02@gamil.com>
2017-09-22 00:24:50 +0530

    Refactor docstrings and other comments format.

--- src/python/user_profile.py
+++ src/python/user_profile.py
@@ -1,20 +1,17 @@
-##############################################################################################################
+##########################################################################
 # Author: chinmayms
 #
 # user_profile: Class which defines the state of a given profile in our social network.
 #
-##############################################################################################################
+##########################################################################
 
 
 class UserProfile:
 
-    """
-    Class Variables
-    user_id: user id of the given profile
+    """Class Variables:
+    user_id: user id of the given profile.
     purchases: list of tuples which consist of the purchase and its timestamp.
-    friends: list of user_ids who are friends of the given profile.
-
-    """
+    friends: list of user_ids who are friends of the given profile."""
 
     # Constructor which defines user_id and empty friends and purchase list.
     def __init__(self, user_id):
@@ -24,12 +21,11 @@ class UserProfile:
         self.friends = []
 
     def get_last_t_purchases(self, num_of_purchases):
+        """Return the last t purchase history of the user. 
 
-        """
         :param num_of_purchases: number of purchases to consider from the latest state. ('T')
-        :return: list of last 'T' purchases.
 
-        """
+        :return: list of last 'T' purchases."""
 
         total_purchase = []
 
@@ -39,58 +35,38 @@ class UserProfile:
         return total_purchase
 
     def get_friend_list(self):
+        """Getter for friend list.
 
-        """
-        Getter for friend list
-        :return: list of friends for the given user_id
-
-        """
+        :return: list of friends for the given user_id."""
 
         return self.friends
 
     def add_friend(self, friend_id):
+        """Adds given user_id to friend list for current profile.
 
-        """
-        Adds given user_id to friend list for current profile.
-        :param friend_id: user_id of profile to be added as friend
-
-        """
+        :param friend_id: user_id of profile to be added as friend."""
 
         self.friends.append(friend_id)
 
     def add_purchase(self, amount, timestamp):
+        """Adds a purchase to purchase list with given amount and timestamp.
 
-        """
-        Adds a purchase to purchase list with given amount and timestamp
-
-        :param amount: amount for purchase to be added
-        :param timestamp: timestamp of purchase made.
-
-        """
+        :param amount: amount for purchase to be added.
+        :param timestamp: timestamp of purchase made."""
 
         self.purchases.append((amount, timestamp))
 
     def remove_friend(self, friend_id):
+        """Remove given user_id from friends list of current profile.
 
-        """
-        Remove given user_id from friends list of current profile
-        :param friend_id:
+        :param friend_id: The friend to be removed."""
 
-        """
         if friend_id in self.friends:
             self.friends.remove(friend_id)
 
     def get_user_id(self):
+        """Getter for user_id of current profile.
 
-        """
-        Getter for user_id of current profile.
-        :return: returns user_id of current profile.
-        """
+        :return: returns user_id of current profile."""
 
         return self.user_id
-
-
-
-
-
-
